3ba454624a1e1c26c22ef038790c0007d8e54885
amoulis <adrien.moulis@gmail.com>
2015-04-11 18:24:11 +0200

    puted back launch deamon

--- main.cpp
+++ main.cpp
@@ -8,11 +8,7 @@ using namespace std;
 
 int main(void)
 {
-
-	std::vector<double> v;
-	v.push_back(4);
-	v.push_back(6);
-	v.push_back(3);
-	cout << CStats::median(v) << endl;
+	CShell ct;
+	ct.DaemonLaunch();
 	return 0;
 }
